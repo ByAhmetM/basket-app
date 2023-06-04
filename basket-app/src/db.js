580f3149723e5dfca4747c6684589cfe8fd25ba8
ByAhmetM <118487365+ByAhmetM@users.noreply.github.com>
2023-06-04 13:39:21 +0300

    Database dışarıya alındı.

---
+++ basket-app/src/db.js
@@ -0,0 +1,34 @@
+const storeItems = [
+  {
+    name: "Fotoğraf Makinesi",
+    src: "./assets/images/camera.jpg",
+    price: 25,
+  },
+  {
+    name: "Kulaklık",
+    src: "./assets/images/headphone.jpg",
+    price: 20,
+  },
+  {
+    name: "Oyun Konsolu",
+    src: "./assets/images/joystick.jpg",
+    price: 10,
+  },
+  {
+    name: "Retro Fotoğraf Makinesi",
+    src: "./assets/images/retro-cam.jpg",
+    price: 10,
+  },
+  {
+    name: "Oyuncak Araba",
+    src: "./assets/images/toy-car.jpg",
+    price: 10,
+  },
+  {
+    name: "Kol Saati",
+    src: "./assets/images/watch.jpg",
+    price: 10,
+  },
+];
+
+export default storeItems;
